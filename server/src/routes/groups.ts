bbb4ba22bda764dce53ea8c53340eee618c0fde5
Tanuj Kumar <150677583+TanujDonkal@users.noreply.github.com>
2026-03-12 21:15:28 -0300

    api: balance calculation + settlement optimization (closes #22) (#28)

--- server/src/routes/groups.ts
+++ server/src/routes/groups.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/auth";
 import { createGroup, addMember, getGroups } from "../controllers/groupController";
+import { getBalances, getSettlements } from "../controllers/settlementController";
 
 const router = Router();
 
@@ -9,5 +10,7 @@ router.use(authenticate);
 router.post("/", createGroup);
 router.get("/", getGroups);
 router.post("/:groupId/members", addMember);
+router.get("/:groupId/balances", getBalances);
+router.get("/:groupId/settlements", getSettlements);
 
 export default router;
